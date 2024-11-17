1268a9a93a9e346c02c9249e13dd20403b8051ff
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-17 16:35:07 +0530

    Create Name&branch.cpp

---
+++ Name&branch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string n,b;
+    int a;
+    cout<<"\nEnter a name\t";
+    cin>>n;
+    cout<<"\nEnter a branch\t";
+    cin>>b;
+    cout<<"\n Enter a rolle number\t";
+    cin>>a;
+    return 0;
+}
