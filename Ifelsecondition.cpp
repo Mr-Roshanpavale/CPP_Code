5bdb1e94d2b5f7821b441df0eb5b3ad6275f040e
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-17 16:48:59 +0530

    Create Ifelsecondition.cpp

---
+++ Ifelsecondition.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"\n enter the any number " ;
+    cin>>n;
+    if(n==0)
+    {
+        cout<<n<<"\n A number is zero";
+
+    }
+    else
+    {
+        cout<<"\n A number is non zero";
+        
+    }
+    return 0;
+}
