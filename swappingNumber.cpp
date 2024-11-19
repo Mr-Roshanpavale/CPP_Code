2d4b271c77245207fbff881af2a32c95fd65b433
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 14:20:16 +0530

    Create swappingNumber.cpp

---
+++ swappingNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    a=10;
+    b=20;
+    c=30;
+    cout<<"\nbefor swapping"<<a<<b;
+    a=b;
+    b=c;
+    cout<<"\nafter swapping"<<a<<b;
+    return 0;
+}
