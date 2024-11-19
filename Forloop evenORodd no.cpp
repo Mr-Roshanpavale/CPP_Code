8084c5bfdf42f75b8b043a9663fba79a47af2874
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 15:12:22 +0530

    Create Forloop evenORodd no.cpp

---
+++ Forloop evenORodd no.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<=10;i++)
+    {
+        if(i%2==0)
+    {
+        cout<<i<<endl;
+    }
+    else
+    {
+        cout<<"odd"<<endl;
+    }
+    }
+    cout<<"-------END-------";
+    return 0;
+}
