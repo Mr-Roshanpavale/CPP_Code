0f1e3fbecb5c74d3841dea24aa794f21d59a0997
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 14:31:29 +0530

    Create Greater&Lessnumber.cpp

---
+++ Greater&Lessnumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"\n the value is greter than";
+    }
+    else
+    {
+        cout<<"\n the value is less than";
+    }
+    return 0;
+}
