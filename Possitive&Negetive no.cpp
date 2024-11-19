35dfd7d2b080d91a67fb107489f76af9eb22251a
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 14:14:27 +0530

    Create Possitive&Negetive no.cpp

---
+++ Possitive&Negetive no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number";
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"Enter a possitive number\n";
+    }
+    else
+    {
+        cout<<"Enrtera negetive number\n";
+    }
+    return 0;
+}
+
