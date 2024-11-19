790e234de5359f613222212f756a664052390058
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 13:53:47 +0530

    Create All ArithmeticOper.cpp

---
+++ All ArithmeticOper.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int a, b, c, d ,e ;
+    cout<<"\n Enter the any numbers ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"\n addition = "<<c;
+    e=a-b;
+    cout<<"\n substraction = "<<e;
+    e=a*b;
+    cout<<"\n multiplication ="<<e;
+    d=a/b;
+    cout<<"\n division = "<<d;
+    c=a%b;
+    cout<<"\n remender = "<<c;
+
+}
