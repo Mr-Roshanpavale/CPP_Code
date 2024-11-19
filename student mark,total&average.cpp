b2b89f1d1bd5af491349eac1a44394e2f213a29b
Roshan Pavale <roshanpawale25@gmail.com>
2024-11-19 14:48:36 +0530

    Create student mark,total&average.cpp

---
+++ student mark,total&average.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name,branch;
+    int m1,m2,m3,total,average;
+    cout<<"Enter a name";
+    cin>>name;
+    cout<<"\n Enter a branch";
+    cin>>branch;
+    cout<<"\n Enter m1,m2,m3";
+    cin>>m1>>m2>>m3;
+    total=m1+m2+m3;
+    average=total/3;
+    cout<<"name"<<name<<endl;
+    cout<<"branch="<<branch<<endl;
+    cout<<"m1="<<m1<<" "<<"m2="<<m2<<" "<<"m3="<<m3<<endl;
+    cout<<"total="<<total<<endl;
+    cout<<"average="<<average;
+}
